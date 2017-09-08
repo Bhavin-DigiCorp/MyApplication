d0a9fcedc45495b10fa4908f46dd7bb6d06b64e2
bhavin.laiya <bhavin2digicorp>
2017-09-08 14:27:53 +0530

    Changes made from clone repo

--- app/src/main/java/com/example/bhavinlaiya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bhavinlaiya/myapplication/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvtest = (TextView) findViewById(R.id.tv);
-        tvtest.setText("bhavin laiya");
+        tvtest.setText("change made from clone");
     }
 }
