32c69f0a4002c9eb0fbab19afda03d2122607fdb
bhavin-DigiCorp <bhavin@digicorp>
2017-09-08 13:09:18 +0530

    Changes made

--- app/src/main/java/com/example/bhavinlaiya/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bhavinlaiya/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvtest = (TextView) findViewById(R.id.tv);
+        tvtest.setText("bhavin laiya");
     }
 }
